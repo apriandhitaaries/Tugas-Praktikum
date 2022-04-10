dd568e9a0653368700323d6f056d895199e1652f
Apriandhita Aries Prayoga <90746400+apriandhitaaries@users.noreply.github.com>
2022-04-10 11:35:43 +0700

    Add files via upload

---
+++ TugasPraktikum1/src/Surat_Kaisar.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Surat_Kaisar {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String kata = input.nextLine();
+        int angka = input.nextInt();
+        System.out.println(pergeseranHuruf(kata, angka)); 
+        input.close();       
+    }
+
+    static String pergeseranHuruf(String kata, int angka){
+        String suratKaisar = ""; 
+        for(int i = 0; i < kata.length(); i++){
+            char huruf = kata.charAt(i);
+            if (huruf >= 'a' && huruf <= 'z'){
+                huruf = (char) (huruf + angka);
+                if(huruf > 'z'){
+                    huruf = (char) (huruf + 'a' - 'z' - 1);
+                } 
+                suratKaisar += huruf;
+
+            } else if (huruf >= 'A' && huruf <= 'Z'){
+                huruf = (char) (huruf + angka);
+                if(huruf > 'Z'){
+                    huruf = (char) (huruf + 'A' - 'Z' - 1);
+                } 
+                suratKaisar += huruf;
+
+            } else {
+                suratKaisar += huruf;
+            }
+
+        }
+        return suratKaisar;
+
+    }
+    
+}
